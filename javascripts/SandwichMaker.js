0861f37c013a53d151b94210cb8eb8f38cadae26
Eric Denton <eric@iamericanartist.com>
2016-07-01 21:02:43 -0500

    starting iife setup

--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -1,5 +1,26 @@
 console.log("<<< SandwichMaker.js loaded >>>");
 
+
+var sandwichCost = Sandwich.getSandwichPrice("cheese");
+var toppingCost = Sandwich.getToppingPrice("captainCrunch");
+var output = document.getElementById("sandwich");
+
+output.innerHTML = sandwichCost + toppingCost;
+
+
+var Sandwich = (function() {
+  var prices = {"pb&j": 2.00, "cheese": .75};
+
+  return {
+    getSandwichPrice: function(sandwich) {
+      return prices[sandwich];
+    }
+  };
+})();
+
+
+
+
 //Kick off our IIFE object here... START:
 var SandwichMaker = (function() {
 var totalPrice = 0;   // Private variable to store the sandwich PRICE
@@ -13,39 +34,3 @@ var totalPrice = 0;   // Private variable to store the sandwich PRICE
     }
   };
 })();
-
-
-
-
-
-// var SolarSystem = (function() {
-// 	var planets = ["Mercury", "Venus", "Earth", "Mars", "Xenon6", "Claire"];
-// 	var planetsLandedUpon = 234;
-// 	// var planetName = "Larry" //PRIVATE AS DEFINED IN THE FUNCTION
-// 	var spacecraft = ["U.S.S. Enterprise", "Voyager", "Cassini", "Yo Mama"];
-
-// 	return {
-// 		//getter for an array of planets that cannot be modified.
-// 		getPlanets: function() {
-// 			return planets;
-// 		},
-// 		//Getter/setter for number of planets that humans have landed people or robots on.
-// 		getPlanetsLandedOn: function() {
-// 			return planetsLandedUpon;
-// 		},
-// 		setPlanetsLandedOnNumber: function(num) {
-// 			planetsLandedUpon = num;
-// 		}, 		
-// 		//public property for holding a last modified date.
-// 		lastModified: Date.now(),
-// 		// planetName:"Fred", //PUBLIC AS DEFINED WITH NO var
-// 		//Getter/setter for spacecraft currently exploring solar system. These should be stored in a private
-// 		getSpacecraft: function(){
-// 			return spacecraft;	
-// 		},
-// 		addSpacecraft: function(newShip){
-// 			spacecraft.push(newShip)
-// 		}
-// 	}
-
-// })();
